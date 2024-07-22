cbe84cb3625047a027ebcebcbac1eb84362e4337
Yohana <yohana.contreras@cleteci.com>
2024-07-22 19:25:04 -0400

    add create and save vectorstore

--- src/text_processing.py
+++ src/text_processing.py
@@ -10,8 +10,8 @@ openai_api_key = os.getenv('OPENAI_API_KEY')
 def get_text_chunks(text):
   text_splitter = CharacterTextSplitter(
     separator="\n",
-    chunk_size=1000,
-    chunk_overlap=200,
+    chunk_size=10000,
+    chunk_overlap=2000,
     length_function=len
   )
   chunks = text_splitter.split_text(text)
@@ -22,3 +22,20 @@ def get_vectorstore(text_chunks):
   vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
   return vectorstore
 
+def save_vectorstore(vectorstore, path):
+  if not os.path.exists(path):
+    os.makedirs(path)
+  vectorstore.save_local(path)
+
+def load_vectorstore(path):
+  return FAISS.load_local(path, embeddings=OpenAIEmbeddings(), allow_dangerous_deserialization=True)
+
+def create_and_save_vectorstore(contenido, path):
+  # Generate the text chunks
+  text_chunks = get_text_chunks(contenido)
+
+  # Creaeate the embeddings and the vectorstore
+  vectorstore = get_vectorstore(text_chunks)
+
+  # save the vectorstore in the disk
+  save_vectorstore(vectorstore, path)
