f836311a42a94d4bbfdd313718973665cd3709d1
Yohana Contreras <yohana.contreras@cleteci.com>
2024-07-22 19:46:15 -0400

    improvements

--- src/text_processing.py
+++ src/text_processing.py
@@ -1,41 +1,48 @@
 import os
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from dotenv import load_dotenv
 
 load_dotenv()
 openai_api_key = os.getenv('OPENAI_API_KEY')
 
 def get_text_chunks(text):
-  text_splitter = CharacterTextSplitter(
-    separator="\n",
-    chunk_size=10000,
-    chunk_overlap=2000,
-    length_function=len
-  )
-  chunks = text_splitter.split_text(text)
-  return chunks
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=10000,
+        chunk_overlap=2000,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
 
 def get_vectorstore(text_chunks):
-  embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-  vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-  return vectorstore
+    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
 
 def save_vectorstore(vectorstore, path):
-  if not os.path.exists(path):
-    os.makedirs(path)
-  vectorstore.save_local(path)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    vectorstore.save_local(path)
 
 def load_vectorstore(path):
-  return FAISS.load_local(path, embeddings=OpenAIEmbeddings(), allow_dangerous_deserialization=True)
+    return FAISS.load_local(path, embeddings=OpenAIEmbeddings(openai_api_key=openai_api_key), allow_dangerous_deserialization=True)
 
 def create_and_save_vectorstore(contenido, path):
-  # Generate the text chunks
-  text_chunks = get_text_chunks(contenido)
+    # Generate the text chunks
+    text_chunks = get_text_chunks(contenido)
 
-  # Creaeate the embeddings and the vectorstore
-  vectorstore = get_vectorstore(text_chunks)
+    # Create the embeddings and the vectorstore
+    vectorstore = get_vectorstore(text_chunks)
 
-  # save the vectorstore in the disk
-  save_vectorstore(vectorstore, path)
+    # Save the vectorstore to disk
+    save_vectorstore(vectorstore, path)
+
+# Define the path where you want to save the vectorstore
+VECTORSTORE_PATH = "vectorstore/"
+
+# Create and save the vectorstore
+cleaned_text = "Aquí va tu texto limpio"  # Asegúrate de definir cleaned_text con el contenido apropiado
+create_and_save_vectorstore(cleaned_text, VECTORSTORE_PATH)
