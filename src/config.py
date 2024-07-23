6d2b6f2f5f264dc1f81ce1bd81a0b3be0664c895
Yohana <yohana.contreras@cleteci.com>
2024-07-22 20:32:56 -0400

    debugging files

--- src/config.py
+++ src/config.py
@@ -3,14 +3,3 @@ from pathlib import Path
 
 VECTORSTORE_PATH = str(Path(__file__).parent.parent / "vectorstore")
 os.makedirs(VECTORSTORE_PATH, exist_ok=True)
-
-# from pathlib import Path 
-
-# # Determine the project root path 
-# PROJECT_ROOT_PATH = Path(__file__).resolve().parent.parent 
- 
-# # Define the data directory 
-# VECTORSTORE_PATH = PROJECT_ROOT_PATH / 'vectorstore'
-
-# Create the data directory if it doesn't exist
-# DATASET_ROOT_PATH.mkdir(parents=True, exist_ok=True)
--- src/text_processing.py
+++ src/text_processing.py
@@ -4,47 +4,46 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
 from dotenv import load_dotenv
-from src import config  # Importar el archivo de configuración
+from src import config
 
-# Cargar las variables de entorno
+# Load environment variables
 load_dotenv()
 
-# Obtener la API key de OpenAI desde las variables de entorno
+# Get the OpenAI API key from environment variables
 openai_api_key = os.getenv('OPENAI_API_KEY')
 
-# Utilizar la ruta VECTORSTORE_PATH desde config.py
+# Use the VECTORSTORE_PATH path from config.py
 VECTORSTORE_PATH = config.VECTORSTORE_PATH
 
-
 def get_text_chunks(text):
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=10000,
-        chunk_overlap=2000,
-        length_function=len
-    )
-    chunks = text_splitter.split_text(text)
-    return chunks
+  text_splitter = CharacterTextSplitter(
+    separator="\n",
+    chunk_size=10000,
+    chunk_overlap=2000,
+    length_function=len
+  )
+  chunks = text_splitter.split_text(text)
+  return chunks
 
 def get_vectorstore(text_chunks):
-    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-    return vectorstore
+  embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+  vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+  return vectorstore
 
 def save_vectorstore(vectorstore, path):
-    if not os.path.exists(path):
-        os.makedirs(path)
-    vectorstore.save_local(path)
+  if not os.path.exists(path):
+    os.makedirs(path)
+  vectorstore.save_local(path)
 
 def load_vectorstore(path):
-    return FAISS.load_local(path, embeddings=OpenAIEmbeddings(openai_api_key=openai_api_key), allow_dangerous_deserialization=True)
+  return FAISS.load_local(path, embeddings=OpenAIEmbeddings(openai_api_key=openai_api_key), allow_dangerous_deserialization=True)
 
 def create_and_save_vectorstore(contenido):
-    # Generar los fragmentos de texto
-    text_chunks = get_text_chunks(contenido)
+  # Generate the text fragments
+  text_chunks = get_text_chunks(contenido)
 
-    # Crear los embeddings y el vectorstore
-    vectorstore = get_vectorstore(text_chunks)
+  # Create the embeddings and the vectorstore
+  vectorstore = get_vectorstore(text_chunks)
 
-    # Guardar el vectorstore en el disco
-    save_vectorstore(vectorstore, VECTORSTORE_PATH)
+  # Save the vectorstore to disk
+  save_vectorstore(vectorstore, VECTORSTORE_PATH)
