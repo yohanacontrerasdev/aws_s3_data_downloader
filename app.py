a270e8ed5031add4fcb688b7b447c2cb3a41f9c3
Yohana Contreras <yohana.contreras@cleteci.com>
2024-07-23 23:01:02 -0400

    Change the import lines to use the new paths from the langchain-openai package

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import os
 import streamlit as st
-from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain_community.chat_models import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings,ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
