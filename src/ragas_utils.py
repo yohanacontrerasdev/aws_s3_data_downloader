be18382b6fa0a3141cd2b254acd23c85c17a3bab
Yohana Contreras <yohana.contrerasg@gmail.com>
2024-07-27 15:32:26 -0400

    Return Both DataFrames

--- src/ragas_utils.py
+++ src/ragas_utils.py
@@ -2,44 +2,42 @@ import pandas as pd
 from datasets import load_dataset
 from ragas import evaluate
 from ragas.metrics import (
-    answer_relevancy,
-    faithfulness,
-    context_recall,
-    context_precision,
-    answer_correctness,
-    answer_similarity
+	answer_relevancy,
+	faithfulness,
+	context_recall,
+	context_precision,
+	answer_correctness,
+	answer_similarity
 )
 
 def get_evaluation():
-    ragas_dataset = load_dataset('json', data_files='data.json')
-    data = ragas_dataset['train']
-    #print("-------- DATA ------------")
-    #print(data)
+	# Load the dataset from a JSON file
+	ragas_dataset = load_dataset('json', data_files='data.json')
+	data = ragas_dataset['train']
 
-    # Metrics
-    metrics=[
-        context_precision,
-        faithfulness,
-        answer_relevancy,
-        context_recall,
-        answer_correctness,
-        answer_similarity
-    ]
+	# Metrics
+	metrics=[
+	context_precision,
+	faithfulness,
+	answer_relevancy,
+	context_recall,
+	answer_correctness,
+	answer_similarity
+	]
 
-    # Evaluation
-    result = evaluate(
-    data,
-    metrics=metrics,
-    raise_exceptions=False
-    )
+	# Evaluation
+	result = evaluate(
+	data,
+	metrics=metrics,
+	raise_exceptions=False
+	)
 
-    # Resultado Global
-    #print(result)
-    df = pd.DataFrame(result, index=[0])
-    res_df = df.transpose()
-    res_df.columns = ["Result"]
-    #st.dataframe(res_df)
+	 # Global Result
+	df = pd.DataFrame(result, index=[0])
+	res_df = df.transpose()
+	res_df.columns = ["Result"]
 
-    # Resultado por pregunta
-    result_df = result.to_pandas()
-    return res_df
+	# Result by question
+	result_df = result.to_pandas()
+
+	return res_df, result_df
