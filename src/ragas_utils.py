7a124956e6be212fa74cb0fe70c39a35638544c2
Estalin Gunza <exgunza@yahoo.com>
2024-07-29 19:24:58 -0500

    EGU-feat: Métodos Utils para datos de Ragas

--- src/ragas_utils.py
+++ src/ragas_utils.py
@@ -1,43 +1,67 @@
 import pandas as pd
-from datasets import load_dataset
-from ragas import evaluate
-from ragas.metrics import (
-	answer_relevancy,
-	faithfulness,
-	context_recall,
-	context_precision,
-	answer_correctness,
-	answer_similarity
-)
+import json
+import os
 
-def get_evaluation():
-	# Load the dataset from a JSON file
-	ragas_dataset = load_dataset('json', data_files='data.json')
-	data = ragas_dataset['train']
+def process_information():
 
-	# Metrics
-	metrics=[
-	context_precision,
-	faithfulness,
-	answer_relevancy,
-	context_recall,
-	answer_correctness,
-	answer_similarity
-	]
+	# Nombre del archivo de texto
+	archivo_txt = os.path.join(os.path.dirname(__file__), 'questions.txt')
 
-	# Evaluation
-	result = evaluate(
-	data,
-	metrics=metrics,
-	raise_exceptions=False
-	)
+	# Lista para almacenar las estructuras de datos
+	datos_json = []
 
-	 # Global Result
-	df = pd.DataFrame(result, index=[0])
-	res_df = df.transpose()
-	res_df.columns = ["Result"]
+	try:
+		# Leer el contenido del archivo de texto
+		with open(archivo_txt, 'r') as file:
+			lines = file.readlines()
 
-	# Result by question
-	result_df = result.to_pandas()
+		# Procesar las líneas del archivo
+		i = 0
+		while i < len(lines):
+			if lines[i].startswith('Question:'):
+				# Extraer la pregunta y la respuesta
+				question = lines[i].strip().split('Question:')[1]
+				ground_truth = lines[i + 1].strip().split('Answer:')[1]
 
-	return res_df, result_df
+				# Crear el diccionario con la estructura requerida
+				dato = {
+					"question": question,
+					"answer": "",
+					"contexts": [""],  
+					"ground_truth": ground_truth
+				}
+
+				# Agregar el diccionario a la lista
+				datos_json.append(dato)
+
+				# Moverse al siguiente par de líneas (Question y Answer)
+				i += 2
+
+			else:
+				i += 1
+
+		# Convertir a DataFrame de Pandas
+		df = pd.DataFrame(datos_json)
+		
+		return df
+	
+	except Exception as e:
+		print(f"Unexpected error: {str(e)}")
+		return None
+
+def create_ragas_data_file(df):
+	# Nombre de la carpeta que deseas crear
+	file_name = "data.json"
+
+	# Ruta completa de la carpeta
+	file_path = os.path.join(os.path.dirname(__file__), file_name)
+
+	json_output = df.to_dict(orient='records')
+
+	try: 
+		# Si se desea guardar el JSON en un archivo
+		with open(file_path, 'w') as json_file:
+			json.dump(json_output, json_file, indent=4)
+		print("The data.json file was created successfully")
+	except Exception as e:
+		print("Error when trying to create data.json file")
