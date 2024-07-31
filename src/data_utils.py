38a44c004ed7ed1c0d528694bb2541422eb9e870
Estalin Gunza <exgunza@yahoo.com>
2024-07-30 19:10:35 -0500

    EGU-feat: Download 20 PDF y uso pymupdf

--- src/data_utils.py
+++ src/data_utils.py
@@ -5,6 +5,7 @@ from llama_parse import LlamaParse
 import pandas as pd
 import nest_asyncio
 import re
+import fitz
 
 nest_asyncio.apply()
 load_dotenv()
@@ -26,19 +27,19 @@ def download_pdfs_from_s3():
   response = s3.list_objects_v2(Bucket=BUCKET_NAME, Prefix=PREFIX)
   data = []
 
-  file_count = 0
+  # file_count = 0
 
   if "Contents" in response:
     for obj in response["Contents"]:
       file_name = obj["Key"]
-      if file_name.endswith("/") or not file_name.endswith(".pdf") or file_count >=2:
+      if file_name.endswith("/") or not file_name.endswith(".pdf"): #or file_count >=20:
         continue
 
       try:
         file_obj = s3.get_object(Bucket=BUCKET_NAME, Key=file_name)
         pdf_content = file_obj['Body'].read()
         data.append({"name": os.path.basename(file_name), "content": pdf_content})
-        file_count +=1
+        # file_count +=1
       except Exception as e:
         print(f"Failed to download {file_name}: {e}")
   else:
@@ -57,10 +58,18 @@ def filter_pdfs_by_years(df):
   filtered_df = df[(df['year'] == max_year) | (df['year'] == min_year)]
   return filtered_df
 
-def extract_text_from_pdf_content(pdf_content, api_key, file_name):
-  document = LlamaParse(result_type="markdown", api_key=api_key).load_data(pdf_content, extra_info={"file_name": file_name})
-  full_text = "".join([t.text for t in document])
-  return full_text
+# def extract_text_from_pdf_content(pdf_content, api_key, file_name):
+#   document = LlamaParse(result_type="markdown", api_key=api_key).load_data(pdf_content, extra_info={"file_name": file_name})
+#   full_text = "".join([t.text for t in document])
+#   return full_text
+
+def extract_text_from_pdf_content(pdf_content):
+    document = fitz.open(stream=pdf_content, filetype="pdf")
+    text = ""
+    for page_num in range(len(document)):
+        page = document.load_page(page_num)
+        text += page.get_text()
+    return text
 
 def download_pdfs_and_convert_to_text():
   LLAMAPARSE_API_KEY = os.getenv("LLAMAPARSE_API_KEY")
@@ -80,8 +89,14 @@ def download_pdfs_and_convert_to_text():
     return pd.DataFrame()
 
   # Extraer texto de los PDFs filtrados
+  # filtered_df['text'] = filtered_df.apply(
+  #     lambda row: extract_text_from_pdf_content(row['content'], LLAMAPARSE_API_KEY, row['name']), axis=1
+  # )
+  # Recuperar los primeros 20 PDFs
+  filtered_df = filtered_df.head(20)
+
   filtered_df['text'] = filtered_df.apply(
-      lambda row: extract_text_from_pdf_content(row['content'], LLAMAPARSE_API_KEY, row['name']), axis=1
+      lambda row: extract_text_from_pdf_content(row['content']), axis=1
   )
 
   return filtered_df
