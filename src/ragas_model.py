36b4fa08320c6c415341752dcdf57ae92b3556ee
lc2101 <lrcroci@gmail.com>
2024-08-05 19:15:06 -0300

    last

--- src/ragas_model.py
+++ src/ragas_model.py
@@ -26,7 +26,7 @@ def get_conversation_chain(vectorstore, template):
     prompt = ChatPromptTemplate.from_template(template)
     llm = ChatOpenAI()
     memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True, output_key='answer')
-    retriever = vectorstore.as_retriever(search_kwargs={'k':4})
+    retriever = vectorstore.as_retriever(search_kwargs={'k':6})
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
         retriever=retriever,
