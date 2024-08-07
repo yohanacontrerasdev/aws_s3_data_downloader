7b72171ba5b0a3fb4a243813bb7950af6d4ddad5
Yohana Contreras <yohana.contrerasg@gmail.com>
2024-08-06 22:58:04 -0400

    improvements

--- src/plots.py
+++ src/plots.py
@@ -17,6 +17,7 @@ def clean_text(text):
   text = re.sub(r'\s*\|\s*', ' ', text)
   text = re.sub(r'(?<!\w)-(?!\w)', ' ', text)
   text = re.sub(r'\s+', ' ', text)
+  text = re.sub(r"[^\w\s|\-]", "", text)
   return text
 
 def analyze_text(cleaned_text, stop_words='english', words_per_segment=50):
