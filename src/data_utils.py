425b40d439b8da1306026df64157a45217105e38
Yohana <yohana.contreras@cleteci.com>
2024-08-08 16:27:20 -0400

    add clean_pdf_in_memory function

--- src/data_utils.py
+++ src/data_utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 import nest_asyncio
 import re
 import fitz
+import io
 
 nest_asyncio.apply()
 load_dotenv()
@@ -71,8 +72,25 @@ def extract_text_with_fitz(pdf_content):
 class LlamaParseAPIKeyMissingError(Exception):
   pass
 
+def clean_pdf_in_memory(pdf_content):
+    try:
+        # Abre el archivo PDF en memoria
+        document = fitz.open(stream=pdf_content, filetype="pdf")
+        # Crea un nuevo archivo PDF en memoria
+        new_pdf_stream = io.BytesIO()
+        document.save(new_pdf_stream)
+        # Devuelve el contenido del nuevo archivo PDF
+        return new_pdf_stream.getvalue()
+    except Exception as e:
+        #logger.error(f"Error cleaning PDF in memory: {e}")
+        return None
+
 # Extract text based on the selected method
 def extract_text(row, use_llamaparse, llamaparse_api_key):
+    cleaned_pdf_content = clean_pdf_in_memory(row['content'])
+    if not cleaned_pdf_content:
+        return None
+    
     if use_llamaparse:
         return extract_text_with_llamaparse(row['content'], llamaparse_api_key, row['name'])
     else:
@@ -97,8 +115,19 @@ def download_pdfs_and_convert_to_text(use_llamaparse=False, llamaparse_api_key=N
     return pd.DataFrame()
 
   # Limit to the first 20 PDFs
-  filtered_df = filtered_df.head(20)
+  #filtered_df = filtered_df.head(20)
 
+  filtered_df = filtered_df.copy()
   filtered_df['text'] = filtered_df.apply(lambda row: extract_text(row, use_llamaparse, llamaparse_api_key), axis=1)
+
+  # Count the number of failed extractions
+  failed_count = filtered_df['text'].isnull().sum()
+
+  # Remove rows where text extraction failed
+  filtered_df = filtered_df.dropna(subset=['text'])
+
+  print(f"Total PDFs processed: {len(df)}")
+  print(f"Total PDFs successfully processed: {len(filtered_df)}")
+  print(f"Total PDFs failed to process: {failed_count}")
 
   return filtered_df
